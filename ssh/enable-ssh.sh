56ceeec9e508633d2a5612793dfdacec6547094b
modi <modi.tamam@gmail.com>
2021-09-28 19:03:40 +0300

    added permissions and ssh directories

---
+++ ssh/enable-ssh.sh
@@ -0,0 +1,8 @@
+On the guest machine:
+  sudo apt-get install openssh-server
+
+On the VBox manager -> machine settings -> network -> advanced -> port forwarding
+
+from the host machine:
+  ssh-copy-id -p 2245 ubuntu@localhost
+  ssh ubuntu@127.0.0.1 -p 2245
